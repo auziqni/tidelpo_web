1545c56890f9d687c78b1409747dd0eeccac51d7
auziqni <65333301+auziqni@users.noreply.github.com>
2024-06-21 00:21:46 +0700

    feat: Image from storage

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>dsfsd</main>
+    <main>
+      <Image
+        src={`${process.env.NEXT_PUBLIC_MEDIA_URL}landing%2Fhero.jpg`}
+        alt="Next.js logo"
+        width={500}
+        height={500}
+        className=""
+      />
+    </main>
   );
 }
